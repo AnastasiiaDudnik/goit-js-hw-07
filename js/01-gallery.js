e3c73507d694d9c31c130d6893ab66664f2500ae
AnastasiiaDudnik <dudniknastya2000@gmail.com>
2022-12-20 14:05:22 +0200

    1st task

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,38 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+
+const markup = galleryItems.map(
+  ({ preview, original, description }) => `<div class="gallery__item">
+  <a class="gallery__link" href="large-image.jpg">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`
+);
+
+gallery.innerHTML = markup.join("");
+
+gallery.addEventListener("click", onImgClick);
+let instance = "";
+
+function onImgClick(event) {
+  event.preventDefault();
+  window.addEventListener("keydown", onEscapePress);
+  instance = basicLightbox.create(`
+    <img src="${event.target.dataset.source}">`);
+
+  instance.show();
+}
+
+function onEscapePress(event) {
+  if (event.code === "Escape") {
+    instance.close();
+    window.removeEventListener("keydown", onEscapePress);
+  }
+}
